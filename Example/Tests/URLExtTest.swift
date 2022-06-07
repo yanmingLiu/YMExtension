12ed0c53c12fc8abbbb8c13414156f262351f8cc
lym <lwb374402328@gmail.com>
2022-06-07 20:24:33 +0800

    0.1.10

--- Example/Tests/URLExtTest.swift
+++ Example/Tests/URLExtTest.swift
@@ -5,6 +5,8 @@
 //  Created by lym on 2022/6/7.
 //  Copyright © 2022 CocoaPods. All rights reserved.
 //
+
+// 在 Swift 2.0 中， Apple 为 app 的测试开了“后门”。现在我们可以通过在测试代码中导入 app 的 target 时，在之前追加 @testable，就可以访问到 app target 中 internal 的内容了。
 @testable import YMExtension
 
 import XCTest
